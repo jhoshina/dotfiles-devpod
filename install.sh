5700eb328b3ef2103f38a903d9aa3026534b428d
Junichi Hoshina <jhoshina@gmail.com>
2026-01-20 21:56:26 +0900

    Fix install.sh

--- install.sh
+++ install.sh
@@ -10,26 +10,28 @@ if ! command -v mise >/dev/null 2>&1; then
   curl https://mise.run | sh
 fi
 
-# stow -v mise
+# mise
 mkdir -p "$HOME/.config/mise"
-ln -sf $SCRIPT_DIR/mise/.config/mise/config.toml ~/.config/mise/config.toml
+ln -sf "$SCRIPT_DIR/mise/.config/mise/config.toml" ~/.config/mise/config.toml
 mise install
-echo "eval \"\$($HOME/.local/bin/mise activate zsh)\"" >> ~/.zshrc
+# echo "eval \"\$($HOME/.local/bin/mise activate zsh)\"" >> ~/.zshrc
 
-# stow -v zsh
-# ln -sf zsh/.zshrc ~/.zshrc
-# ln -sf zsh/.zshenv ~/.zshenv
-echo "export EDITOR=nvim" >> ~/.zshenv
-echo "export PAGER=bat" >> ~/.zshenv
+# zsh
+# ln -sf "$SCRIPT_DIR/zsh/.zshrc" ~/.zshrc
+ln -sf "$SCRIPT_DIR/zsh/.zshenv" ~/.zshenv
 
-# stow -v nvim
+# nvim
 if [ ! -d "$HOME/.config/nvim" ]; then
   git clone https://github.com/LazyVim/starter ~/.config/nvim
   rm -rf ~/.config/nvim/.git
 fi
-ln -sf "$SCRIPT_DIR/nvim/.config/nvim/lua/plugins/colorscheme.lua" ~/.config/nvim/lua/plugins/colorscheme.lua
-ln -sf "$SCRIPT_DIR/nvim/.config/nvim/lua/plugins/dap.lua" ~/.config/nvim/lua/plugins/dap.lua
-ln -sf "$SCRIPT_DIR/nvim/.config/nvim/lua/plugins/neotest.lua" ~/.config/nvim/lua/plugins/neotest.lua
 ln -sf "$SCRIPT_DIR/nvim/.config/nvim/lua/config/options.lua" ~/.config/nvim/lua/config/options.lua
+ln -sf "$SCRIPT_DIR/nvim/.config/nvim/lua/plugins/colorscheme.lua" ~/.config/nvim/lua/plugins/colorscheme.lua
+# ln -sf "$SCRIPT_DIR/nvim/.config/nvim/lua/plugins/dap.lua" ~/.config/nvim/lua/plugins/dap.lua
+# ln -sf "$SCRIPT_DIR/nvim/.config/nvim/lua/plugins/neotest.lua" ~/.config/nvim/lua/plugins/neotest.lua
 
-# stow -v tmux
+# tmux
+mkdir -p "~/.config/tmux"
+mkdir -p "~/.local/share/tmux/plugins"
+git clone https://github.com/tmux-plugins/tpm ~/.local/share/tmux/plugins/tpm
+
