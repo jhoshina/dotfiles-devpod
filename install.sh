71ccaf8a106a66dc9ab456b8274c6e83d1fa2017
Junichi Hoshina <jhoshina@gmail.com>
2026-01-22 18:17:08 +0900

    Update path

--- install.sh
+++ install.sh
@@ -12,26 +12,26 @@ fi
 
 # mise
 mkdir -p "$HOME/.config/mise"
-ln -sf "$SCRIPT_DIR/mise/.config/mise/config.toml" ~/.config/mise/config.toml
+ln -sf "$SCRIPT_DIR/mise/.config/mise/config.toml" "$HOME/.config/mise/config.toml"
 mise install
-# echo "eval \"\$($HOME/.local/bin/mise activate zsh)\"" >> ~/.zshrc
+# echo "eval \"\$($HOME/.local/bin/mise activate zsh)\"" >> "$HOME/.zshrc"
 
 # zsh
-# ln -sf "$SCRIPT_DIR/zsh/.zshrc" ~/.zshrc
-ln -sf "$SCRIPT_DIR/zsh/.zshenv" ~/.zshenv
+# ln -sf "$SCRIPT_DIR/zsh/.zshrc" "$HOME/.zshrc"
+ln -sf "$SCRIPT_DIR/zsh/.zshenv" "$HOME/.zshenv"
 
 # nvim
 if [ ! -d "$HOME/.config/nvim" ]; then
-  git clone https://github.com/LazyVim/starter ~/.config/nvim
-  rm -rf ~/.config/nvim/.git
+  git clone https://github.com/LazyVim/starter "$HOME/.config/nvim"
+  rm -rf "$HOME/.config/nvim/.git"
 fi
-ln -sf "$SCRIPT_DIR/nvim/.config/nvim/lua/config/options.lua" ~/.config/nvim/lua/config/options.lua
-ln -sf "$SCRIPT_DIR/nvim/.config/nvim/lua/plugins/colorscheme.lua" ~/.config/nvim/lua/plugins/colorscheme.lua
-# ln -sf "$SCRIPT_DIR/nvim/.config/nvim/lua/plugins/dap.lua" ~/.config/nvim/lua/plugins/dap.lua
-# ln -sf "$SCRIPT_DIR/nvim/.config/nvim/lua/plugins/neotest.lua" ~/.config/nvim/lua/plugins/neotest.lua
+ln -sf "$SCRIPT_DIR/nvim/.config/nvim/lua/config/options.lua" "$HOME/.config/nvim/lua/config/options.lua"
+ln -sf "$SCRIPT_DIR/nvim/.config/nvim/lua/plugins/colorscheme.lua" "$HOME/.config/nvim/lua/plugins/colorscheme.lua"
+# ln -sf "$SCRIPT_DIR/nvim/.config/nvim/lua/plugins/dap.lua" "$HOME/.config/nvim/lua/plugins/dap.lua"
+# ln -sf "$SCRIPT_DIR/nvim/.config/nvim/lua/plugins/neotest.lua" "$HOME/.config/nvim/lua/plugins/neotest.lua"
 
 # tmux
-mkdir -p "~/.config/tmux"
-mkdir -p "~/.local/share/tmux/plugins"
-git clone https://github.com/tmux-plugins/tpm ~/.local/share/tmux/plugins/tpm
+mkdir -p "$HOME/.config/tmux"
+mkdir -p "$HOME/.local/share/tmux/plugins"
+git clone https://github.com/tmux-plugins/tpm "$HOME/.local/share/tmux/plugins/tpm"
 
